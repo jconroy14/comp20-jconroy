7d448f22c08f73f4572e3d9e42b7ac9e31e8f1d9
jonathanconroy <jonathanconroy14@gmail.com>
2019-06-14 00:16:23 -0400

    notuber: identify closest car

--- notuber/map.js
+++ notuber/map.js
@@ -1,4 +1,6 @@
+const MILES_PER_METER = 0.000621371;
 var map;
+
 var vehicleData = [
     {id: "mXfkjrFw", location: {lat: 42.3453, lng: -71.0464}},
     {id: "nZXB8ZHz", location: {lat: 42.3662, lng: -71.0621}},
@@ -11,7 +13,7 @@ var vehicleData = [
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 42.352271, lng: -71.05524200000001},
-        zoom: 14
+        zoom: 11
     });
 
     vehicleData.forEach(function(car) {
@@ -20,29 +22,40 @@ function initMap(){
     });
 
     navigator.geolocation.getCurrentPosition(function(position) {
-        let lat = position.coords.latitude;
-        let lng = position.coords.longitude;
+        let my_loc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         let my_marker = new google.maps.Marker({
-            position: {lat, lng},
+            position: my_loc,
             map: map
         })
-        markCars(lat,lng)
+        markCars(my_loc)
     });
 }
 
-function markCars(lat, lng) {
+function markCars(my_loc) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "https://hans-moleman.herokuapp.com/rides");
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send("username=ciuyoepn&lat=" + lat + "&lng=" + lng);
+    xhr.send("username=ciuyoepn&lat=" + my_loc.lat() + "&lng=" + my_loc.lng());
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             cars = JSON.parse(xhr.responseText);
+            let minDist = null;
+            let closestCar = null;
 
             cars.forEach(function(car) {
-                let position = {lat: car.lat, lng: car.lng}
+                let position = new google.maps.LatLng(car.lat, car.lng);
                 let marker = new google.maps.Marker({position, map, icon: "car.png"});
+
+                let test = google.maps.geometry.spherical.computeDistanceBetween(my_loc, position);
+                let dist = google.maps.geometry.spherical.computeDistanceBetween(my_loc, position);
+                if (!minDist || dist < minDist) {
+                    minDist = dist * MILES_PER_METER;
+                    closestCar = car;
+                }
             });
+            console.log(closestCar);
+            console.log(minDist);
+
         }
     }
 }
