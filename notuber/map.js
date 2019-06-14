5cd6023a694d6fb87546d04527d0da376ada201d
jonathanconroy <jonathanconroy14@gmail.com>
2019-06-14 00:26:21 -0400

    notuber: Added polyline to closest car + debugging

--- notuber/map.js
+++ notuber/map.js
@@ -2,12 +2,12 @@ const MILES_PER_METER = 0.000621371;
 var map;
 
 var vehicleData = [
-    {id: "mXfkjrFw", location: {lat: 42.3453, lng: -71.0464}},
+/*    {id: "mXfkjrFw", location: {lat: 42.3453, lng: -71.0464}},
     {id: "nZXB8ZHz", location: {lat: 42.3662, lng: -71.0621}},
     {id: "Tkwu74WC", location: {lat: 42.3603, lng: -71.0547}},
     {id: "5KWpnAJN", location: {lat: 42.3472, lng: -71.0802}},
     {id: "uf5ZrXYw", location: {lat: 42.3663, lng: -71.0544}},
-    {id: "VMerzMH8", location: {lat: 42.3542, lng: -71.0704}},
+    {id: "VMerzMH8", location: {lat: 42.3542, lng: -71.0704}},*/
 ];
 
 function initMap(){
@@ -39,22 +39,26 @@ function markCars(my_loc) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             cars = JSON.parse(xhr.responseText);
-            let minDist = null;
+            let minDistMeters = null;
             let closestCar = null;
 
             cars.forEach(function(car) {
                 let position = new google.maps.LatLng(car.lat, car.lng);
                 let marker = new google.maps.Marker({position, map, icon: "car.png"});
 
-                let test = google.maps.geometry.spherical.computeDistanceBetween(my_loc, position);
-                let dist = google.maps.geometry.spherical.computeDistanceBetween(my_loc, position);
-                if (!minDist || dist < minDist) {
-                    minDist = dist * MILES_PER_METER;
-                    closestCar = car;
+                let distMeters = google.maps.geometry.spherical.computeDistanceBetween(my_loc, position);
+                console.log(dist);
+                if (!minDistMeters || distMeters < minDistMeters) {
+                    minDistMeters = distMeters;
+                    closestCar = position;
                 }
             });
-            console.log(closestCar);
-            console.log(minDist);
+
+            let path = new google.maps.Polyline({
+                path: [my_loc, closestCar],
+                strokeColor: '#FF0000',
+                map: map
+            });
 
         }
     }
